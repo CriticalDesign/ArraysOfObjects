ca314c040257b4d20538cf044bc5fc33a710745e
Aaron Langille <aaron.langille@gmail.com>
2024-02-07 09:03:19 -0500

    Add project files.

---
+++ ArraysOfObjects/Die.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysOfObjects
+{
+    internal class Die
+    {
+
+        private int _sideUp;
+        private int _numSides;
+
+        public Die(int numSides)
+        {
+            _numSides = numSides;
+            Roll();
+        }
+
+        public void Roll()
+        {
+            Random rng = new Random();
+            _sideUp = rng.Next(1, _numSides + 1);
+        }
+
+        public int GetSideUp()
+        {
+            return _sideUp;
+        }
+    }
+}
---
+++ ArraysOfObjects/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace ArraysOfObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Die> bagOfDice = new List<Die>();
+            Console.Write("How many dice?: ");
+            int numDice = int.Parse(Console.ReadLine());    
+
+            for(int i = 0; i < numDice; i++)
+            {
+                //Die tempDie = new Die(6);
+                //bagOfDice.Add(tempDie);
+
+                bagOfDice.Add(new Die(4));
+            }
+
+            for(int i = 0; i < bagOfDice.Count; i++)
+            {
+                bagOfDice[i].Roll();
+            }
+
+            for(int i = 0; i < bagOfDice.Count; i++)
+            {
+                if (bagOfDice[i].GetSideUp() == 1)
+                {
+                    //Console.WriteLine("Removing: " + i);
+                    //bagOfDice.Remove(bagOfDice[i]);
+                    bagOfDice[i] = null;
+                }
+            }
+
+            int sumOfDice = 0;
+            for (int i = 0; i < bagOfDice.Count; i++)
+            {
+                if (bagOfDice[i] != null)
+                {
+                    int sideUp = bagOfDice[i].GetSideUp();
+                    sumOfDice += sideUp;
+                    Console.WriteLine((i + 1) + " is showing " + sideUp);
+                }
+            }
+            Console.WriteLine("Sum of dice is: " + sumOfDice);
+
+            if(bagOfDice.Count == 0)
+            {
+                Console.WriteLine("Game Over");
+            }
+
+        }
+    }
+}
